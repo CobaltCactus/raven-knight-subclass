e7f9f14e86cec388e00fe3eefe398eceba8cbede
CobaltCactus <ao3.cobaltcactus@gmail.com>
2026-06-20 19:18:04 -0500

    Backup at Sat Jun 20 19:18:04 CDT 2026

---
+++ README.md
@@ -0,0 +1,4 @@
+# Raven Knight Subclass
+Adds the Raven Knight Fighter Subclass by king-starman (with permission).\
+[Source](https://www.reddit.com/r/UnearthedArcana/comments/lgsn58/fighter_raven_knight_version_2_serve_as_one_of/)\
+[Description, screenshots, and downloads on mod.io](https://mod.io/g/baldursgate3/m/TBD)
--- backup.bash
+++ backup.bash
@@ -8,10 +8,10 @@ set -e;
 # MOD_SUBDIR_NAME="CircleOfTheSpores_db7e15fd-b2fc-b159-4bbd-1baab34d8c3a"
 # Look in "C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects"
 # for names of mods you have already started
-MOD_SUBDIR_NAME=""
+MOD_SUBDIR_NAME="Raven_Knight_Fighter_Subclass_9844f8eb-4096-5a59-f6aa-8e6f1f2efa0f"
 
 # This is the MinGW64 path to a Steam install of the toolkit
-BG3_DATA="/c/Program Files (x86)/Steam/steamapps/common/Baldurs Gate 3/Data"
+BG3_DATA="/d/Program Files (x86)/Steam/steamapps/common/Baldurs Gate 3/Data"
 
 # These are set according to "Understanding the Mod Locations", as of Nov 2024, from
 # https://mod.io/g/baldursgate3/r/getting-started-creating-a-new-mod
